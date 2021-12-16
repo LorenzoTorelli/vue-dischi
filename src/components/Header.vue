02e23d2edfba5ad01f748ab2e0afdb066ec1288a
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-12-16 16:38:13 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="header"></div>
+</template>
+
+<script>
+export default {
+    name:"Header",
+}
+</script>
+
+<style lang="scss" scoped>
+    #header {
+        width: 100vw;
+        height: 50px;
+        background-color: $Main-color;
+    }
+</style>>
